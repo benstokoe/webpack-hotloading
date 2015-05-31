addd0d0149bcdd332fed776f2cf4461b4b4da497
Ben Stokoe <benstokoe1@googlemail.com>
2015-05-31 13:50:11 +0100

    Got hot reloading working!

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,25 @@
+var webpack = require('webpack');
+
 module.exports = {
-    entry: './js/app.js',
+    entry: [
+        'webpack-dev-server/client?http://0.0.0.0:3000', // WebpackDevServer host and port
+        'webpack/hot/only-dev-server',
+        './js/app' // Your appʼs entry point
+    ],
     output: {
-        path: './dist/',
-        filename: 'bundle.js'
+        path: '/dist/',
+        filename: 'bundle.js',
+        publicPath: '/static/'
     },
     module: {
-        loaders: [
-            { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader' }
-        ]
-    }
+        loaders: [{
+            test: /\.js$/,
+            exclude: /node_modules/,
+            loaders: ['react-hot', 'babel-loader']
+        }]
+    },
+    plugins: [
+        new webpack.HotModuleReplacementPlugin(),
+        new webpack.NoErrorsPlugin()
+    ]
 }
